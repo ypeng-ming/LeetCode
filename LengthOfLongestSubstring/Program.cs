bbaa527ec286cbcbb543a3d2fc6c624207a44c14
ypeng-ming <pengyongming91@gmail.com>
2023-06-06 10:53:23 +0800

    update and add

---
+++ LengthOfLongestSubstring/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LengthOfLongestSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcabcbb";//"";//dvdf
+            LengthOfLongestSubstring(s);
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            #region answer1
+
+            //HashSet<char> set = new HashSet<char>();
+            //int max = 0;
+            //int size = s.Length;
+            //int left = 0;
+            //int right = 0;
+            //while (right < size)
+            //{
+            //    if (!set.Contains(s[right]))
+            //    {
+            //        set.Add(s[right]);
+            //        right++;
+            //    }
+            //    else
+            //    {
+            //        set.Remove(s[left]);
+            //        left++;
+            //    }
+
+            //    max = Math.Max(max, set.Count);
+            //}
+
+            //return max;
+
+            #endregion
+            int [] last = new int[256];
+            //for (int i = 0; i < 256; i++)
+            //{
+            //    last[i] = -1;
+            //}
+
+            int max = 0;
+            int start = 0;
+            int size = s.Length;
+
+            for (int i = 0; i < size; i++)
+            {
+                int index = s[i];//get ascii code
+                start = Math.Max(start, last[index] );
+                max = Math.Max(max, i - start + 1);
+                last[index] = i+1;
+            }
+
+            return max;
+        }
+    }
+}
--- MaxArea/Program.cs
+++ MaxArea/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MaxArea
 {
@@ -6,7 +7,42 @@ namespace MaxArea
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] x = new[] {1, 8, 6, 2, 5, 4, 8, 3, 7};
+            MaxArea(x);
+        }
+
+        //double pointer
+        public static  int MaxArea(int[] height)
+        {
+                 int max = 0;
+                 int size = height.Length;
+                 int left = 0, right = size - 1;
+                 while (left<right)
+                 {
+                     max = Math.Max(max, (right - left) * Math.Min(height[left], height[right]));
+                     if (height[left]>height[right])
+                     {
+                         --right;
+                     }
+                     else
+                     {
+                         ++left;
+                     }
+                 }
+
+                 return max;
+        }
+
+        private static bool Pass(Dictionary<int, int> temp, int height, int width)
+        {
+            if (temp.ContainsKey(height)&& temp[height]==width||
+                temp.ContainsKey(width)&&temp[width]==height)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
+
 }
